c5a52970de77fd0f9b922f43d9b8ddf89a0b4723
michellemnyg <michelle.mnyg@gmail.com>
2026-04-29 16:31:28 +0800

    Revert "base setup & index.php's login & register link changed"
    This reverts commit dfa9f0fc918fa8769a75ba70e43e5b1e48bde357.

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -5,9 +5,4 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-
-$routes->setAutoRoute(true);
-
-$routes->get('/', 'Page::index');
-$routes->get('login', 'Page::login');
-$routes->get('register', 'Page::register');
+$routes->get('/', 'Home::index');
