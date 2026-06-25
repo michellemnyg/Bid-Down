53e07434b119c7b743efe18444b973c3bd86042d
michellemnyg <michelle.mnyg@gmail.com>
2026-06-25 21:29:20 +0800

    Fix duplicate style tag breaking layout

--- biddown/resources/views/layouts/app.blade.php
+++ biddown/resources/views/layouts/app.blade.php
@@ -14,7 +14,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
 
     <style>
-        <style>
 :root{
     --primary:#8b5e3c;
     --primary-soft:#f4ece4;
